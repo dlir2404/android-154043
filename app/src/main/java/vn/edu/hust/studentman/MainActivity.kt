a69434ae272f713c68b163ac966f8e64350674c4
Dlir <dinhlinh.work24@gmail.com>
2024-11-19 23:38:23 +0700

    student man v2

--- app/src/main/java/vn/edu/hust/studentman/MainActivity.kt
+++ app/src/main/java/vn/edu/hust/studentman/MainActivity.kt
@@ -1,20 +1,40 @@
 package vn.edu.hust.studentman
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var addNewDialog: Dialog;
+  private lateinit var launcher: ActivityResultLauncher<Intent>;
+  private lateinit var students: MutableList<StudentModel>;
+  private lateinit var studentAdapter: StudentAdapter;
+
+  private var deletedStudent: StudentModel? = null;
+  private var editingStudent: StudentModel? = null;
+  private var deletedPosition: Int = -1;
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val students = mutableListOf(
+    students = mutableListOf(
       StudentModel("Nguyễn Văn An", "SV001"),
       StudentModel("Trần Thị Bảo", "SV002"),
       StudentModel("Lê Hoàng Cường", "SV003"),
@@ -37,15 +57,17 @@ class MainActivity : AppCompatActivity() {
       StudentModel("Lê Văn Vũ", "SV020")
     )
 
-    val studentAdapter = StudentAdapter(students, this)
+    val value1 = intent.getIntExtra("param3", 0)
 
-    findViewById<RecyclerView>(R.id.recycler_view_students).run {
-      adapter = studentAdapter
-      layoutManager = LinearLayoutManager(this@MainActivity)
-    }
+    studentAdapter = StudentAdapter(students, this)
+
+    val listView = findViewById<ListView>(R.id.list_view);
+    listView.adapter = studentAdapter
+
+    registerForContextMenu(listView);
 
     //add new
-    val addNewDialog = Dialog(this)
+    addNewDialog = Dialog(this)
     addNewDialog.setContentView(R.layout.add_new_dialog)
     addNewDialog.window?.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
 
@@ -56,7 +78,7 @@ class MainActivity : AppCompatActivity() {
       val mssv = editMssv.text.toString()
 
       students.add(StudentModel(hoten, mssv));
-      studentAdapter.notifyItemInserted(students.size - 1);
+      studentAdapter.notifyDataSetChanged();
 
       addNewDialog.dismiss()
     }
@@ -65,8 +87,102 @@ class MainActivity : AppCompatActivity() {
       addNewDialog.dismiss()
     }
 
-    findViewById<Button>(R.id.btn_add_new).setOnClickListener {
-      addNewDialog.show();
+    launcher = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+      if (it.resultCode == Activity.RESULT_OK) {
+        val newName = it.data?.getStringExtra("newName")
+        val newId = it.data?.getStringExtra("newId")
+
+        if (editingStudent == null) {
+          Snackbar.make(findViewById(android.R.id.content), "Invalid student position", Snackbar.LENGTH_LONG).show()
+          return@registerForActivityResult
+        } else {
+          if (newName != null) {
+            editingStudent!!.studentName = newName
+          }
+
+          if (newId != null) {
+            editingStudent!!.studentId = newId
+          }
+
+          studentAdapter.notifyDataSetChanged()
+        }
+      }
     }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.appbar_menu, menu)
+    return true;
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.action_add -> {
+        addNewDialog.show()
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
+  override fun onCreateContextMenu(
+    menu: ContextMenu?,
+    v: View?,
+    menuInfo: ContextMenu.ContextMenuInfo?
+  ) {
+    menuInflater.inflate(R.menu.student_context_menu, menu);
+    super.onCreateContextMenu(menu, v, menuInfo);
+  }
+
+  override fun onContextItemSelected(item: MenuItem): Boolean {
+    val pos = (item.menuInfo as AdapterContextMenuInfo).position;
+
+    when (item.itemId) {
+      R.id.action_edit -> {
+        moveToEditActivity(students[pos]);
+      }
+      R.id.action_delete -> {
+        showDeleteDialog(students[pos], pos)
+      }
+    }
+    return super.onContextItemSelected(item)
+  }
+
+  private fun moveToEditActivity(student: StudentModel) {
+    val intent = Intent(this, EditStudentActivity::class.java);
+    intent.putExtra("studentName", student.studentName)
+    intent.putExtra("studentId", student.studentId)
+    editingStudent = student
+
+    launcher.launch(intent)
+  }
+
+  private fun showDeleteDialog(student: StudentModel, position: Int) {
+    AlertDialog.Builder(this)
+      .setTitle("Delete student?")
+      .setPositiveButton("OK") { _, _ ->
+        deletedStudent = student
+        deletedPosition = position
+
+        students.removeAt(position)
+        studentAdapter.notifyDataSetChanged()
+
+        val rootView = findViewById<View>(android.R.id.content)
+        Snackbar.make(rootView, "Deleted successfully", Snackbar.LENGTH_LONG)
+          .setAction("Undo") {
+            if (deletedStudent !== null && deletedPosition != -1) {
+              students.add(deletedPosition, deletedStudent!!)
+              studentAdapter.notifyDataSetChanged()
+
+              deletedStudent = null;
+              deletedPosition = -1;
+
+              Snackbar.make(rootView, "Undo successfully", Snackbar.LENGTH_LONG).show()
+            }
+          }
+          .show()
+      }
+      .setNegativeButton("Cancel", null)
+      .show()
   }
 }
--- app/src/main/java/vn/edu/hust/studentman/StudentAdapter.kt
+++ app/src/main/java/vn/edu/hust/studentman/StudentAdapter.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -18,92 +19,24 @@ import com.google.android.material.snackbar.Snackbar
 class StudentAdapter(
   val students: MutableList<StudentModel>,
   val context: Context
-): RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
-  private var deletedStudent: StudentModel? = null;
-  private var deletedPosition: Int = -1;
-  class StudentViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    val textStudentName: TextView = itemView.findViewById(R.id.text_student_name)
-    val textStudentId: TextView = itemView.findViewById(R.id.text_student_id)
-    val imageEdit: ImageView = itemView.findViewById(R.id.image_edit)
-    val imageRemove: ImageView = itemView.findViewById(R.id.image_remove)
-  }
+): BaseAdapter() {
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
-    val itemView = LayoutInflater.from(parent.context).inflate(R.layout.layout_student_item,
-       parent, false)
-    return StudentViewHolder(itemView)
-  }
+  override fun getCount(): Int = students.size;
 
-  override fun getItemCount(): Int = students.size
+  override fun getItem(position: Int): StudentModel = students[position]
 
-  override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-    val student = students[position]
+  override fun getItemId(position: Int): Long = position.toLong();
 
-    holder.textStudentName.text = student.studentName
-    holder.textStudentId.text = student.studentId
-    holder.imageEdit.setOnClickListener{
-      showEditDialog(student, position)
-    }
-    holder.imageRemove.setOnClickListener {
-      showDeleteDialog(student, position, holder)
-    }
-  }
+  override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_student_item, parent, false);
 
-  private fun showEditDialog(student: StudentModel, position: Int) {
-    val editDialog = Dialog(context)
-    editDialog.setContentView(R.layout.edit_dialog)
-    editDialog.window?.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+    val studentName = view.findViewById<TextView>(R.id.text_student_name)
+    val studentId = view.findViewById<TextView>(R.id.text_student_id)
 
-    val editHoten = editDialog.findViewById<EditText>(R.id.edit_hoten)
-    val editMssv = editDialog.findViewById<EditText>(R.id.edit_mssv)
-    val btnOk = editDialog.findViewById<Button>(R.id.button_ok)
-    val btnCancel = editDialog.findViewById<Button>(R.id.button_cancel)
+    val student = students[position];
+    studentName.text = student.studentName;
+    studentId.text = student.studentId;
 
-    editHoten.setText(student.studentName)
-    editMssv.setText(student.studentId)
-
-    btnOk.setOnClickListener {
-      student.studentName = editHoten.text.toString()
-      student.studentId = editMssv.text.toString()
-
-      notifyItemChanged(position)
-
-      editDialog.dismiss()
-    }
-
-    btnCancel.setOnClickListener {
-      editDialog.dismiss()
-    }
-
-    editDialog.show()
-  }
-
-  private fun showDeleteDialog(student: StudentModel, position: Int, holder: StudentViewHolder) {
-    AlertDialog.Builder(context)
-      .setTitle("Delete student?")
-      .setPositiveButton("OK") { _, _ ->
-        deletedStudent = student
-        deletedPosition = position
-
-        students.removeAt(position)
-        notifyItemRemoved(position)
-
-        val rootView = (context as Activity).findViewById<View>(android.R.id.content)
-        Snackbar.make(rootView, "Deleted successfully", Snackbar.LENGTH_LONG)
-          .setAction("Undo") {
-            if (deletedStudent !== null && deletedPosition != -1) {
-              students.add(deletedPosition, deletedStudent!!)
-              notifyItemInserted(deletedPosition)
-
-              deletedStudent = null;
-              deletedPosition = -1;
-
-              Snackbar.make(rootView, "Undo successfully", Snackbar.LENGTH_LONG).show()
-            }
-          }
-          .show()
-      }
-      .setNegativeButton("Cancel", null)
-      .show()
+    return view;
   }
 }
