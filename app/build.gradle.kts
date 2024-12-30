8f0a2ec8e923e2e224da9d0fc3ab1da717b2f17a
Dlir <dinhlinh.work24@gmail.com>
2024-12-30 23:20:18 +0700

    fix: add plugin and update Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
+  id("kotlin-kapt")
 }
 
 android {
@@ -20,29 +21,33 @@ android {
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
   }
+
   kotlinOptions {
     jvmTarget = "11"
   }
 }
 
 dependencies {
-
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
   implementation(libs.androidx.activity)
   implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.room.common)
-  implementation(libs.androidx.room.runtime)
+
+  implementation(libs.androidx.room.ktx)
+  kapt(libs.androidx.room.compiler)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
-  annotationProcessor(libs.androidx.room.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.kotlin.kapt)
-}
-
-dependencies {
-  implementation(libs.androidx.room.runtime)
-  kapt(libs.androidx.room.compiler)
-  implementation(libs.androidx.room.ktx)
-  implementation(libs.kotlinx.coroutines.android)
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
 }
